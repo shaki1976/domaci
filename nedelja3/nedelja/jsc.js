a25e3b87d287c3d44e9b9aa1fe48ff2f9ddb1f61
shaki1976 <amjsavic@gmail.com>
2021-08-14 16:12:29 +0200

    kompletiran domaci

--- nedelja3/nedelja/jsc.js
+++ nedelja3/nedelja/jsc.js
@@ -1,5 +1,5 @@
 //1. Napisati funkciju koja ispisuje sve elemente datog niza koji su deljivi sa 5
-
+console.log("---------------------------- zad 1");
 {
   let arr = [2, 5, 14, 20, 25, 33, 15];
   function deljiviSa5(niz) {
@@ -9,7 +9,7 @@
 
   deljiviSa5(arr);
 }
-
+console.log("-------------------------------------- zad 2");
 //2. Napraviti objekat pokemon koji sadrži sledeće informacije: (Napravite makar 4 različita pokemona)
 // * Ime
 // * Vrsta
@@ -17,6 +17,7 @@
 // * Karakteristike (objekat sa informacijama : napad (broj), odbrana (broj), brzina (broj))
 
 //(Napraviti niz od ovih objekata)
+// pokusacu preko konstruktorske fje, ne da bih se pravio vazan, nego ucenja radi
 
 function Pokemon(ime, vrsta, sposobnosti, karakteristike) {
   this.ime = ime;
@@ -24,8 +25,12 @@ function Pokemon(ime, vrsta, sposobnosti, karakteristike) {
   this.sposobnosti = sposobnosti;
   this.karakteristike = karakteristike;
 
-  this.pojedinacneSposobnosti = function () {
-    return this.sposobnosti.join(",");
+  this.zbirKarakteristika = function () {
+    return (
+      this.karakteristike.napad +
+      this.karakteristike.odbrana +
+      this.karakteristike.brzina
+    );
   };
 }
 
@@ -35,44 +40,87 @@ function Karakteristike(napad, odbrana, brzina) {
   this.brzina = brzina;
 }
 
-let Pikachu = new Pokemon(
-  "Pikachu",
-  "Elektricni",
-  ["staticki elektricitet"],
-  new Karakteristike(40, 30, 60)
-);
-
 let Charizard = new Pokemon(
   "Charizard",
   "vatreni",
   ["vatra", "letenje"],
-  new Karakteristike(50, 50, 60)
+  new Karakteristike(50, 50, 70)
 );
 
 let Venusaur = new Pokemon(
   "Venusaur",
   "seme",
   ["velicina", "trava", "otrov"],
-  new Karakteristike(50, 50, 50)
+  new Karakteristike(60, 50, 40)
 );
-
+let Pikachu = new Pokemon(
+  "Pikachu",
+  "Elektricni",
+  ["staticki elektricitet"],
+  new Karakteristike(40, 30, 90)
+);
 let Sandshrew = new Pokemon(
   "Sandshrew",
   "mis",
   ["veo peska", "kupanje"],
-  new Karakteristike(50, 50, 30)
+  new Karakteristike(60, 50, 30)
 );
 
-let pokemoni = [Pikachu, Charizard, Venusaur, Sandshrew];
+let pokemoni = [Charizard, Venusaur, Pikachu, Sandshrew];
 
+console.log(pokemoni);
+console.log("-------------------------------------- zad 3");
 //3. Napraviti funkciju koja prima niz gore napravljenih objekata i vraća jedan niz sposobnosti svih pokemona
 function nizSposobnosti(pokemoni) {
   let nizSposobnosti = [];
-  for (const pokemon in pokemoni) {
-    console.log(pokemon);
-    // nizSposobnosti.push(
-    //   pokemon.pojedinacneSposobnosti().bind(pokemon).split(","));
-  }
+  pokemoni.forEach((pokemon) => {
+    let sposobnosti = pokemon["sposobnosti"];
+    for (const sposobnost of sposobnosti) {
+      nizSposobnosti.push(sposobnost);
+    }
+  });
+  return nizSposobnosti;
 }
-nizSposobnosti(pokemoni);
+
+console.log(nizSposobnosti(pokemoni));
 // console.log(Pikachu.pojedinacneSposobnosti());
+
+console.log("-------------------------------------- zad 4");
+//4. Sortirati pokemone po brzini, rastuće.
+
+function sortirajPoBrzini(pokemoni) {
+  pokemoni.sort((a, b) =>
+    a.karakteristike.brzina > b.karakteristike.brzina
+      ? 1
+      : b.karakteristike.brzina > a.karakteristike.brzina
+      ? -1
+      : 0
+  );
+}
+sortirajPoBrzini(pokemoni);
+console.log(pokemoni);
+
+console.log("-------------------------------------- zad 5");
+//5. Napraviti funkciju koja prima niz pokemona, poredi pokemone po jačini i vraća kao pobednika onog koji ima najveću jačinu napada.
+
+function poJaciniNapada(niz) {
+  // pokusao sam da do resenja dodjem i pomocu filter i reduce ali dosad nisam uspeo, trenutno mi samo ovo radi
+  let pobednik = niz[0];
+  // u slucaju da dva pokemona imaju isti napad pobednik je onaj koji ima veci zbir svih karakteristika
+  for (const pokemon of niz) {
+    zbirKarakteristika =
+      pokemon.karakteristike.napad +
+      pokemon.karakteristike.odbrana +
+      pokemon.karakteristike.brzina;
+    if (pokemon.karakteristike.napad > pobednik.karakteristike.napad)
+      pobednik = pokemon;
+    else if (
+      pokemon.karakteristike.napad === pobednik.karakteristike.napad &&
+      pokemon.zbirKarakteristika() > pobednik.zbirKarakteristika()
+    )
+      pobednik = pokemon;
+  }
+  return pobednik;
+}
+
+console.log(poJaciniNapada(pokemoni));
