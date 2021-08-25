b04c044bc7764e35dd012c8b869a9ec74f1c1991
shaki1976 <amjsavic@gmail.com>
2021-08-25 06:18:20 +0200

    pocetak

---
+++ nedelja5/sreda/jsc.js
@@ -0,0 +1,17 @@
+// Napisati validaciju za formu koja sadrzi polja:
+// input-text(name, surname, password, passwordConfirm)
+// checkbox(courses)
+// select(city)
+// radiobutton(gender)
+// Za polje name => obavezno, minimum 5 karaktera, maksimum 15
+// Za polje surname => obavezno, minimum 5 karaktera, maksimum 20
+// Za polje password => obavezno, minimum 8 karaktera, da sadrzi broj i veliko slovo
+// Za polje passwordConfirm => da je isto kao i password
+// Za polje course => obavezno
+
+//klikom na register ako validacija ne prodje ispisati greske ispod polja (za svako polje ponaosob)
+//ako validacija prodje, kreirati objekat user ubaciti ga u niz users i ispisati ga na ekranu (ispisati ceo niz)
+//kada se kreira user: 1. ime i prezime mora biti trimovano i setovati samo prvo slovo kao veliko a sva ostala da budu mala
+//nakon svakog unosa potrebno je ispisati novog user-a i obrisati sve inpute (staviti ih na pocetne vrednosti)
+
+//HTML I CSS po sopstvenom izboru!!!!!!!!!!!!!!!
