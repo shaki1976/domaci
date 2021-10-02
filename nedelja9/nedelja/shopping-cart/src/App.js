39c6ab36406622a91a8b6780f7ec35baa8d274e3
shaki1976 <amjsavic@gmail.com>
2021-10-02 11:44:10 +0200

    nastavak

--- nedelja9/nedelja/shopping-cart/src/App.js
+++ nedelja9/nedelja/shopping-cart/src/App.js
@@ -1,7 +1,23 @@
 import { React, useEffect, useState } from "react";
+import ProductForm from "./components/ProductForm";
+import { getAllProducts } from "./service";
 
 function App() {
-  return <div className="App"></div>;
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+getAllProducts().then((res) => {
+  // console.log(res)
+  setProducts(res.data);
+})
+  }, [])
+
+  return <div className="App">
+    <h2>add new product</h2>
+    <br />
+    <ProductForm setProducts={setProducts} />
+  </div>;
 }
 
 export default App;
--- nedelja9/nedelja/shopping-cart/src/components/ProductForm.jsx
+++ nedelja9/nedelja/shopping-cart/src/components/ProductForm.jsx
@@ -1,4 +1,47 @@
-const ProductForm = () => {
-  return <div></div>;
+import { useState } from "react";
+
+const ProductForm = ({setProducts}) => {
+  const [inputedName, setInputedName] = useState("");
+  const [inputedPrice, setInputedPrice] = useState(0);
+  const [inputedInfo, setInputedInfo] = useState("");
+
+  return (
+    <div>
+      <form>
+        <p>name:</p>
+        <input
+          type="text"
+          placeholder="e.g.)snickers 50g"
+          onChange={(e) => {
+            setInputedName(e.target.value);
+          }}
+        />
+        <p>price:</p>
+        <input type="number" min="0.01" step="0.01" placeholder="e.g.)53.99" onChange={
+          (e) => {
+            setInputedPrice(Number(e.target.value))
+          }
+        } />
+        <p>info:</p>
+        <input type="text" placeholder="e.g.)candy" onChange={
+          (e) => {
+            setInputedInfo(e.target.value)
+          }
+        } />
+        <div><button onClick={
+          (e) => {
+            setProducts((prev) => {
+              const prod = {
+                name: inputedName,
+                info: inputedInfo,
+                price: inputedPrice
+              }
+              return [...prev, prod]
+            })
+          }
+        } >Submit</button></div>
+      </form>
+    </div>
+  );
 };
 export default ProductForm;
